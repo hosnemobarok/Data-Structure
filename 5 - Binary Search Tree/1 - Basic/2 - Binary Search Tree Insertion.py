62d53d6762d23cf90fdf7817d30553d2bbbc3053
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2021-02-23 09:40:40 +0600

    Add files via upload

---
+++ 5 - Binary Search Tree/1 - Basic/2 - Binary Search Tree Insertion.py
@@ -0,0 +1,83 @@
+
+
+class Node:
+
+    def __init__(self, data):
+        self.val = data
+        self.left_child = None
+        self.right_child = None
+
+
+def Inorder_Tree_Traversal(root):
+    if root:
+        Inorder_Tree_Traversal(root.left_child)
+        print(root.val, end=" ")
+        Inorder_Tree_Traversal(root.right_child)
+
+
+def insert_Node(root, key):
+    if root is None:
+        return Node(key)
+
+    else:
+        if root.val == key:
+            return root
+
+        elif root.val < key:
+            root.right_child = insert_Node(root.right_child, key)
+
+        else:
+            root.left_child = insert_Node(root.left_child, key)
+
+    return root
+
+
+
+if __name__ == '__main__':
+    """
+    Before Binary Tree:
+
+                     ____50____
+                   /            \
+               ___20___      ___60___
+              /        \    /        \
+            10         25  45        70
+           /  \
+          1  None
+
+    """
+    root = Node(50)
+
+    root.left_child = Node(20)
+    root.right_child = Node(60)
+
+    root.left_child.left_child = Node(10)
+    root.left_child.right_child = Node(25)
+
+    root.right_child.left_child = Node(45)
+    root.right_child.right_child = Node(70)
+
+    root.left_child.left_child.left_child = Node(1)
+
+    # call inorder function
+    print("Print Binary Tree Inorder Traversal:")
+    Inorder_Tree_Traversal(root)
+
+    """
+    After Binary Tree:
+
+                         ____50____
+                       /            \
+                   ___20___      ___60___
+                  /        \    /        \
+                10         25  50        70
+               /  \            /
+              1  None         45
+
+    """
+
+    # call insert_Node function
+    insert_Node(root, 9)
+
+    print("\n\nPrint Binary Tree Inorder Traversal:")
+    Inorder_Tree_Traversal(root)
