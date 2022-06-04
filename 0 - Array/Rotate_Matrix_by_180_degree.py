50969a38f266a7fd04c04a4dd5fdfb7483725a8e
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2022-06-04 19:34:50 +0600

    Sat 4 Jun 7:34 PM

---
+++ 0 - Array/Rotate_Matrix_by_180_degree.py
@@ -0,0 +1,19 @@
+def Inplace_rotate_square_matrix():
+    
+    m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    
+    res = []
+    for i in range(len(m[0])-1, -1, -1):
+        
+        sub = []
+        for j in range(len(m)-1, -1, -1):
+            sub += [m[i][j]]
+
+        res.append(sub)
+
+    print(res)
+        
+        
+Inplace_rotate_square_matrix()
+
+
